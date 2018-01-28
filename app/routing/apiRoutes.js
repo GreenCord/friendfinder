eb59d370b8b04014576e080c00b97f2ba3cf4349
GreenCord <steve@nevizen.com>
2018-01-28 09:10:34 -0500

    POST + response testing

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,5 +5,8 @@ module.exports = function(app){
 		.get('/api/friends',(req,res)=>res.json(friends))
 		.post('/api/friends',(req,res)=>{
 			console.log('Post request received on /api/friends');
+			console.log(req.body);
+			// TODO compare to friends and find match
+			res.json(req.body); // do not leave this it's dumb
 		});
 };
