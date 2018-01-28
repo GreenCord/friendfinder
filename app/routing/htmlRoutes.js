efeea28e5c369283885dbc6583651ae1dcf33508
GreenCord <steve@nevizen.com>
2018-01-28 08:29:50 -0500

    Home Page and Styles

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,6 +2,8 @@ var path = require('path');
 
 module.exports = function(app){
 	app
+		.get('/assets/css/style.css',(req,res)=>res.sendFile(path.join(__dirname,'../public/assets/css/style.css')))
+		.get('/assets/img/:filename',(req,res)=>res.sendFile(path.join(__dirname,'../public/assets/img/' + req.params.filename)))
 		.get('/survey',(req,res)=>res.sendFile(path.join(__dirname,'../public/survey.html')))
 		.get('*',(req,res)=>res.sendFile(path.join(__dirname,'../public/home.html')));
 };
