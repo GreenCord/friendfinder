ac280d64a29a7666a582aa08b5546e9751fc9fe5
GreenCord <steve@nevizen.com>
2018-01-28 09:05:05 -0500

    Survey and POST

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,6 +4,7 @@ module.exports = function(app){
 	app
 		.get('/assets/css/style.css',(req,res)=>res.sendFile(path.join(__dirname,'../public/assets/css/style.css')))
 		.get('/assets/img/:filename',(req,res)=>res.sendFile(path.join(__dirname,'../public/assets/img/' + req.params.filename)))
+		.get('/assets/js/:filename',(req,res)=>res.sendFile(path.join(__dirname,'../public/assets/js/' + req.params.filename)))
 		.get('/survey',(req,res)=>res.sendFile(path.join(__dirname,'../public/survey.html')))
 		.get('*',(req,res)=>res.sendFile(path.join(__dirname,'../public/home.html')));
 };
